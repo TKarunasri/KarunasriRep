9fe352661334d18ea82b706ef6e7eb10878ee710
TKarunasri <karuna120973@gmail.com>
2023-08-01 14:30:59 +0530

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This is line added by Karunasri.</h1>
+      <h1>This is line added by Karunasri from local system.</h1>
       <h1>This is line added by other team members.</h1>
     </div>
   );
